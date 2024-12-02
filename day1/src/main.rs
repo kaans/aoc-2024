3777b77042d3e1a7e59dd475efb7b4498abe6dc7
Jens <jens.saalmueller@codecentric.de>
2024-12-02 18:46:46 +0100

    feat[day]: implement part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,16 +2,23 @@ use std::fs;
 
 fn main() {
     part1();
+    part2();
 }
 
-fn part1() {
+fn get_input() -> (Vec<u32>, Vec<u32>) {
     let content = fs::read_to_string("input.txt").expect("Something went wrong reading the file") ;
 
-    let (mut left, mut right): (Vec<_>, Vec<_>)= content.lines()
+    let (left, right): (Vec<_>, Vec<_>)= content.lines()
         .map(|line| line.split("   "))
         .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
         .map(|(left, right)| (left.parse::<u32>().unwrap(), right.parse::<u32>().unwrap()))
         .unzip();
+
+    (left, right)
+}
+
+fn part1() {
+    let (mut left, mut right) = get_input();
 
     left.sort();
     right.sort();
@@ -19,4 +26,12 @@ fn part1() {
     let result = left.iter().zip(right.iter()).map(|(left, right)| left.abs_diff(*right)).sum::<u32>();
 
     println!("Result of part 1: {}", result);
+}
+
+fn part2() {
+    let (left, right) = get_input();
+
+    let result: u32  = left.iter().map(|l| right.iter().filter(|r| l == *r).count() as u32 * *l).sum();
+
+    println!("Result of part 2: {}", result);
 }
