2dcee074ecfb49dbeb2024679f042706aae3f4ad
Jens <jens.saalmueller@codecentric.de>
2024-12-04 08:59:13 +0100

    feat[day2]: implement part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
-fn main()
-{
-    part1();
+fn main() {
+    let result_part1 = part1();
+    part2(result_part1);
 }
 
 fn get_input() -> Vec<Vec<i32>> {
@@ -10,21 +10,69 @@ fn get_input() -> Vec<Vec<i32>> {
 
     println!("Total input elements: {}", input.lines().count());
 
-    input.lines().map(|line| line.split_whitespace().map(|entry| entry.parse::<i32>().unwrap()).collect::<Vec<i32>>()).collect()
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|entry| entry.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+        })
+        .collect()
 }
 
-fn part1() {
+fn validate(report: &Vec<i32>) -> bool {
+    let diffs = report.windows(2).map(|e| e[0] - e[1]).collect::<Vec<_>>();
+    let count_level = diffs.iter().filter(|e| e.abs() > 3 || **e == 0).count();
+    let count_decl = diffs
+        .windows(2)
+        .filter(|e| e[0].signum() != e[1].signum())
+        .count();
+
+    count_decl + count_level == 0
+}
+
+fn part1() -> usize {
     let input = get_input();
 
-    let a: Vec<_> = input.iter()
-        .map(|report| report
-            .windows(2)
-            .map(|e| e[0] - e[1])
-            .collect::<Vec<_>>()
-        )
-        .filter(|report| report.iter().filter(|e| e.abs() > 3 || **e == 0).count() == 0)
-        .filter(|report| report.windows(2).filter(|e| e[0].signum() != e[1].signum()).count() == 0)
-        .collect();
+    let zero = input.iter().filter(|report| {
+        if validate(&report) {
+            return true;
+        }
 
-    println!("Result part 1: {}", a.len());
+        false
+    }).collect::<Vec<_>>();
+
+    println!("RESULT part 1: {}", zero.len());
+
+    zero.len()
+}
+
+fn part2(result_part1: usize) {
+    let input = get_input();
+
+    let more = input.iter().filter(|report| {
+        if validate(&report) {
+            return false;
+        }
+
+        true
+    }).collect::<Vec<_>>();
+
+    let res = more.iter().filter(|report| {
+        for i in 0..report.len() {
+            let mut reportsub = report.to_vec();
+            reportsub.remove(i);
+            if validate(&reportsub) {
+                return true;
+            }
+        }
+
+        false
+    }).collect::<Vec<_>>();
+
+    println!("Additional safe reports: {}", res.len());
+
+    println!("RESULT party 2: {}", res.len() + result_part1);
+
+    return;
 }
